5bc0718bfacb211f6866b575bc666a6f2b3bd4c5
MahiroCreative <135081677+itukikunmeuu@users.noreply.github.com>
2025-04-10 16:25:27 +0900

    up

---
+++ UnityExamC/Assets/Scripts/Camera.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Camera : MonoBehaviour
+{
+    //ゲームの開始位置にするかどうか
+    public bool SetStartPos = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if(SetStartPos)
+        {
+
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
